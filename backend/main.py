7d04c7c6c8d3c4134036da870c197f19cdc72788
sushelan <nerdysushi06@gmail.com>
2025-01-02 17:55:39 -0800

    finished

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ def create_contact():
         return {
             
             jsonify({"message": "You must include a first name, last name and email"}),
-            400,
+            400
         }
 
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
@@ -29,7 +29,35 @@ def create_contact():
         return jsonify({"message": str(e)}), 400
     
     return jsonify({"message": "User created!"}), 201
+
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
     
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "User updated."}), 200
+    
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "user not found"}), 404
+    
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted"}), 200
 
 if __name__ == "__main__":
 
